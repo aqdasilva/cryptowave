aea555a77a373627ec9ede3f2bd0a54e11ba3e2c
Anthony Quinn DaSilva <a6dasilva@student.bridgew.edu>
2023-06-14 20:14:07 -0400

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const handleClick = () => {
+    // Play video
+    const video = document.getElementById('video');
+    video.play();
+
+    // Redirect to the next screen
+    setTimeout(() => {
+      // Add your redirection logic here
+      // For example, you can use React Router: history.push('/next-screen');
+    }, 3000); // Redirect after 3 seconds
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" onClick={handleClick}>
+        Crypto Heat Wave
       </header>
+      <video id="video" style={{ display: 'none' }} controls>
+        <source src="heatwave.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
